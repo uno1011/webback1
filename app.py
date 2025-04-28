9af131a61ccf432a633616eef53fc247abfad393
uno1011 <151750618+uno1011@users.noreply.github.com>
2025-04-28 18:56:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,5 +15,11 @@ def receive_data():
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use Render’s port or default 5000
+    port = int(os.environ.get("PORT", 5000))  
     app.run(host='0.0.0.0', port=port)
+
+
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) 
